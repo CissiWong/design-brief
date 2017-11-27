dc0a4eb02b00818115e8db8fe9b0c881c65fb0ef
Cecilia Wong <cissi.wong@gmail.com>
2017-11-27 18:44:35 +0100

    inner text box

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -28,60 +28,70 @@ class App extends React.Component {
             <h1>Interesting ideas that set your mind in motion</h1>
               <p>Hear directly from the people that know it best. From tech to politics to creativity and more - whatever your interest, we've got you covered.</p>
           </article>
-
         <div className="img-playground"></div>
-
           <div className="btn-section">
               <button className="start-btn">Get started!</button>
               <button className="learn-btn">Learn more!</button>
           </div>
       </section>
 
-      <section className="blog-section">
+  <section className="blog-section">
 
         <article className="blog" id="first-blog">
           <div className="blog-image" id="first-image"></div>
-           <div className="first-text">
+           <div className="text-container">
+            <div className="text" id="first-text">
               <h2>The Boy who tamed the sea</h2>
-                <p className="blog-text">lorem ipsum</p>
-                  <div className="writer-image" id="first-writer"></div>
-                    <p className="writer">Per Persson</p>
-                      <img className="bookmark" src="/images/bookmark.png" alt="bookmark image"></img>
-            </div>
+                <p>lorem ipsum</p>
+                  </div>
+                    <div className="writer-image" id="first-writer">
+                      <p>Per Persson</p>
+                        <div className="bookmark"></div>
+                      </div>
+                  </div>
         </article>
 
-          <article className="blog" id="second-blog">
-            <div className="blog-image" id="second-image"></div>
-              <div className="second-text">
-               <h2>The role of human emotion in the future of transport</h2>
-                  <p className="blog-text">lorem ipsum</p>
-                    <div className="writer-image" id="second-writer"></div>
-                      <p className="writer">Per Persson</p>
-                        <img className="bookmark" src="/images/bookmark.png" alt="bookmark image"></img>
-              </div>
-          </article>
+      <article className="blog" id="second-blog">
+        <div className="blog-image" id="second-image"></div>
+           <div className="text-container">
+            <div className="text" id="second-text">
+              <h2>The role of human emotion in the future of transport</h2>
+                <p>lorem ipsum</p>
+                  </div>
+                    <div className="writer-image" id="second-writer">
+                      <p>Per Persson</p>
+                      <div className="bookmark"></div>
+                    </div>
+                </div>
+              </article>
 
-      <article className="blog" id="third-blog">
-        <div className="blog-image" id="third-image"></div>
-          <div className="third-text">
-            <h2>The march of the losers</h2>
-                <p className="blog-text">lorem ipsum</p>
-                  <div className="writer-image" id="third-writer"></div>
-                    <p className="writer">Per Persson</p>
-                      <img className="bookmark" src="/images/bookmark.png" alt="bookmark image"></img>
-          </div>
-        </article>
+        <article className="blog" id="third-blog">
+            <div className="blog-image" id="third-image"></div>
+             <div className="text-container">
+              <div className="text" id="third-text">
+                  <h2>The march of the losers</h2>
+                    <p>lorem ipsum</p>
+                      </div>
+                        <div className="writer-image" id="third-writer">
+                          <p>Per Persson</p>
+                          <div className="bookmark"></div>
+                          </div>
+                            </div>
+                </article>
 
         <article className="blog" id="fourth-blog">
           <div className="blog-image" id="fourth-image"></div>
-            <div className="fourth-text">
+           <div className="text-container">
+            <div className="text" id="fourth-text">
               <h2>The burning season</h2>
-                <p className="blog-text">lorem ipsum</p>
-                  <div className="writer-image" id="fourth-writer"></div>
-                    <p className="writer">Per Persson</p>
-                      <img className="bookmark" src="/images/bookmark.png" alt="bookmark image"></img>
-              </div>
-        </article>
+                <p>lorem ipsum</p>
+                  </div>
+                      <div className="writer-image" id="fourth-writer">
+                        <p>Per Persson</p>
+                          <div className="bookmark"></div>
+                            </div>
+                              </div>
+                      </article>
       </section>
     </div>
   </div>
